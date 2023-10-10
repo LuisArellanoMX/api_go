fdca04e4d889088c458acd75bca4fdd21d916e74
LuisArellanoMX <le.arellanohernandez@ugto.mx>
2023-10-09 19:41:55 -0600

    Version estable, cambios a db.go para traer las variables de entorno nuevas

---
+++ common/db.go
@@ -0,0 +1,42 @@
+package common
+
+import (
+	"context"
+	"errors"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var db *mongo.Database
+
+func GetDBCollection(col string) *mongo.Collection {
+	return db.Collection(col)
+}
+
+func InitDB() error {
+	// Conecatandose a  MongoDB
+	uri := os.Getenv("CONNECTION_STRING")
+	if uri == "" {
+		return errors.New("error al obtener la variable de entorno 'CONNECTION_STRING' ")
+	}
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
+	if err != nil {
+		return err
+	}
+
+	// Inicializando la base de datos
+	db_name := os.Getenv("DB_NAME")
+	if db_name == "" {
+		return errors.New("error al obtener la variable de entorno 'DB_NAME' ")
+	}
+
+	db = client.Database(db_name)
+
+	return nil
+}
+
+func CloseDB() error {
+	return db.Client().Disconnect(context.Background())
+}
